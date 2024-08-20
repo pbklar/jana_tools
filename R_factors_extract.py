e8e47c12ab8aeea844de678acf9a20690ae28b65
Paul <103244714+pbklar@users.noreply.github.com>
2024-08-20 17:24:39 +0200

    Bug with older ref files (Jana2006)

--- R_factors_extract.py
+++ R_factors_extract.py
@@ -20,6 +20,12 @@ __version__ = "20 August 2024"
 
 JANA_ref_files = []
 
+print("- - - - - - - - - - ")
+print(" J A N A  R factors:")
+print("- - - - - - - - - - ")
+print("This program reads in JANA refinement *.ref files and gives an overview on selected parameters.")
+print()
+
 for folder in arguments:
     folder = Path(folder)
     if folder.is_dir():
@@ -30,31 +36,43 @@ for folder in arguments:
             print(N_files, 'JANA refinements found in', folder)
             
 if not JANA_ref_files:
-    print("No JANA refinements found.")
-    print("Hit ENTER to search recursively in subfolders of the current working directory:")
+    print("Current working directory:")
     folder = Path(".")
     print(folder.resolve())
-    
+    print("User input required!")
+    print("- If you just hit ENTER (empty input), all subfolders of the working directory are searched for JANA refinement files.")
+    print("- If you input the path to a folder with JANA refinement file(s), only refinements of that folder are read.")
+    print('PATH:')
     choice = input()
     
-    # Recursive search for ref files
-    JANA_ref_files = list(folder.glob('**/*.ref'))
+    if choice.strip() == '':
+        # Recursive search for ref files
+        JANA_ref_files = list(folder.glob('**/*.ref'))
+    else:
+        folder = Path(choice)
+        if folder.is_dir():
+            JANA_ref_files = list(folder.glob('*.ref'))
     
     print(len(JANA_ref_files), 'JANA refinements found.')
-    
 
-
-
+# PREPARE
 empty = []
-
-output = ''
-output_tmp = ''
-
 this_folder = False
 
+# this variable will be printed at the end
+output = ''
+
+# this variable collects information from each relevant folder
+output_tmp = ''
+
+# READ
 for file in JANA_ref_files:
+    multiblock = False
+    Nobs = 0
+    
     if file.parent != this_folder:
         if output_tmp.count("\n") > 2:
+            # if last folder contained useful ref files, use the generated output
             output += output_tmp + '\n'
             
         this_folder = file.parent
@@ -63,13 +81,10 @@ for file in JANA_ref_files:
         print('       ', '.' * len(str(file.parent)), '\n')
         output_tmp = ' > > ' + str(file.parent) + '\\ < < \n'
         output_tmp += "File                              Parameters    Nobs    Nall  GOFobs  GOFall    Robs    Rall   wRall\n"
-        
+    
     print('READING:', file.name)
     with open(file, "r") as fh:
-        Lines = fh.readlines()
-        
-    multiblock = False
-    Nobs = 0
+        Lines = fh.readlines() 
     
     for i, line in enumerate(Lines):
         if line.startswith("Last screen information window"):
@@ -77,10 +92,10 @@ for file in JANA_ref_files:
             j = 0
             while counter:
                 LL = Lines[i+j]
-                if "-" in LL:
+                if LL[0] == "-":
                     counter -= 1
                     print(LL, end='')
-                elif "|" in Lines[i+j]:
+                elif LL[0] == '|':
                     print(LL, end='')
                     if LL.startswith('|R factors'):
                         # SINGLE BLOCK
@@ -93,7 +108,7 @@ for file in JANA_ref_files:
                         GoF_data = LL
                     elif LL.startswith('|R(obs)='):
                         R_data = LL
-                elif Lines[i+j].startswith('***'): # should not be needed ...
+                elif LL.startswith('***'): # avoid infinite loop
                     break
                 j += 1
 
@@ -115,12 +130,13 @@ for file in JANA_ref_files:
             
             if not multiblock:
                 break
+                
         elif line.startswith('* R-factors overview *'):
+            # only needed if multiple blocks are present in refinement
             BlockID = 0
             for line in Lines[i+2:]:
                 data = line.split()
                 if line.startswith('Block'):
-                    print('BLOCK', line)
                     BlockID = line.split('->')[0]
                 elif line.startswith('#') and ":" in line:
                     BlockID = "Block" + line[1:].split(":")[0]
